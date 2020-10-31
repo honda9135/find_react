673bc623d49417c42ca2d42d6a242f6ab4a1827a
soimmy <dayanbiefu7@gmail.com>
2020-10-31 17:14:41 +0900

    Bottom navgation fixed bottom

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -3,16 +3,18 @@ import React, { Component } from 'react'
 export default class BottomNav extends Component {
     render() {
         return (
-            <nav>
-                <div className="nav-wrapper row white">
-                    <ul className="">
-                        <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>home</i></a></li>
-                        <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>person</i></a></li>
-                        <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>refresh</i></a></li>
-                        <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>face</i></a></li>
-                    </ul>
-                </div>
-            </nav>
+            <div className="navbar-fixed" style={{bottom:"0", position:"absolute"}}>
+                <nav>
+                    <div className="nav-wrapper row white">
+                        <ul>
+                            <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>home</i></a></li>
+                            <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>person</i></a></li>
+                            <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>refresh</i></a></li>
+                            <li className="col s3"><a href="./"><i className="material-icons" style={{color:"grey"}}>face</i></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
